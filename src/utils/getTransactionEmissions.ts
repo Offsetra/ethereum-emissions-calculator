f6487e9691f6e89683e3c8b2fa335479d4363d7e
Brendan McGill <brendanmc6@gmail.com>
2022-09-22 14:44:11 -0700

    safer floating point math

--- src/utils/getTransactionEmissions.ts
+++ src/utils/getTransactionEmissions.ts
@@ -1,6 +1,10 @@
 import { EmissionsFactor, TransactionData } from "../types";
 
-/** returns [emissionsInKGCO2, gasUsed] */
+/**
+ * for each txn, finds the entry in emissions factor table
+ * (where the timestamp is greater than or equal to txn timestamp)
+ * @returns [emissionsInKGCO2, gasUsed]
+ */
 export const getTransactionEmissions = (
   txns: TransactionData[],
   emissionsFactorTable: EmissionsFactor[]
@@ -8,27 +12,23 @@ export const getTransactionEmissions = (
   let datePointer = 0;
   return txns.reduce(
     (prev, tx) => {
-      const txnIsOlder =
-        parseInt(tx.timeStamp) > emissionsFactorTable[datePointer].timestamp;
-      while (txnIsOlder) {
+      // if the timestamp is older, try the next entry
+      while (
+        parseInt(tx.timeStamp) > emissionsFactorTable[datePointer].timestamp
+      ) {
         if (datePointer === emissionsFactorTable.length - 1) {
           break;
         }
         datePointer++;
       }
+      const txnEmissions =
+        parseInt(tx.gasUsed) *
+        emissionsFactorTable[datePointer].emissionsFactor;
+      const sum = prev[0] + txnEmissions; // danger, floating point errors 0.30000000000000004
       const val: [number, number] = [
-        prev[0] +
-          parseInt(tx.gasUsed) *
-            emissionsFactorTable[datePointer].emissionsFactor,
+        Number(sum.toFixed(3)), // take nearest gram 0.00300000000000004 -> 0.003
         prev[1] + parseInt(tx.gasUsed),
       ];
-      console.log(
-        "fetched value",
-        val,
-        tx.timeStamp,
-        emissionsFactorTable[datePointer].timestamp,
-        emissionsFactorTable[datePointer].emissionsFactor
-      );
       return val;
     },
     [0, 0]
